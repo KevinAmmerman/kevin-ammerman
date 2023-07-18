9f1b5d1d4c424d147e2ec33cfbae6087a9921a3d
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-07-18 12:27:47 +0200

    added footer and privacy declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { MenuComponent } from './menu/menu.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { FooterComponent } from './footer/footer.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SlideshowComponent,
     MenuComponent,
-    ImprintComponent
+    ImprintComponent,
+    FooterComponent,
+    DataProtectionComponent
   ],
   imports: [
     BrowserModule,
